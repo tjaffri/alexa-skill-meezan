d4d79934370a7aab5bd23887bd9dfd0efbbbc071
axis-tip <info@axis.com.pk>
2016-09-26 17:21:02 -0700

    CI: Tests

---
+++ test/lifecycle-test.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+import { handler as Skill } from '..';
+import { Request } from 'alexa-annotations';
+
+test('LaunchRequest', t => {
+  const event = Request.launchRequest().build();
+
+  return Skill(event).then(response => {
+    t.deepEqual(response, {
+      version: '1.0',
+      response: {
+        shouldEndSession: true,
+        outputSpeech: { type: 'PlainText', text: 'Meezan launched!' }
+      }
+    });
+  });
+});
