1da3e953114ddc8ce7d2146f16c64752d40ed408
Taqi Jaffri <tjaffri@gmail.com>
2016-10-05 00:42:24 -0700

    CI: Test fixes

--- test/chapterInfoByNameIntent-test.js
+++ test/chapterInfoByNameIntent-test.js
@@ -3,7 +3,7 @@ import { Request } from 'alexa-annotations';
 import { handler as Skill } from '..';
 
 test('ChapterInfoByNameIntent for Surah Al-Faatiha', t => {
-  const event = Request.intent('ChapterInfoByNameIntent', { MEEZAN_CHAPTERNAME: 'Al-Faatiha' }).build();
+  const event = Request.intent('ChapterInfoByNameIntent', { ChapterName: 'Al-Faatiha' }).build();
 
   return Skill(event).then(response => {
     const expectedText = 'Chapter 1, Surah Al-Faatiha, which means The Opening, has 7 verses. It is a Meccan revelation.';
@@ -19,7 +19,7 @@ test('ChapterInfoByNameIntent for Surah Al-Faatiha', t => {
 });
 
 test('ChapterInfoByNameIntent for Surah Yusuf ', t => {
-  const event = Request.intent('ChapterInfoByNameIntent', { MEEZAN_CHAPTERNAME: 'Yusuf' }).build();
+  const event = Request.intent('ChapterInfoByNameIntent', { ChapterName: 'Yusuf' }).build();
 
   return Skill(event).then(response => {
     const expectedText = 'Chapter 12, Surah Yusuf, which means Joseph, has 111 verses. It is a Meccan revelation.';
